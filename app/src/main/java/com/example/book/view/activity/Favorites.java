e1cf11d733a1cfb9cc84bc5f36bd4518958c9d0e
Festive <27864684+mash-reza@users.noreply.github.com>
2019-09-07 11:26:05 +0430

    favorites list backgroun added from assets

--- app/src/main/java/com/example/book/view/activity/Favorites.java
+++ app/src/main/java/com/example/book/view/activity/Favorites.java
@@ -1,9 +1,12 @@
 package com.example.book.view.activity;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.ImageView;
 
 import com.example.book.R;
 import com.example.book.adapter.ChapterAdapter;
@@ -14,13 +17,26 @@ import java.io.IOException;
 
 public class Favorites extends AppCompatActivity {
 
+    private static final String TAG = "Favorites";
+
+    ImageView background;
     RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
+
+        background = findViewById(R.id.favorites_list_background);
+        try {
+            background.setImageDrawable(Drawable.createFromStream(this.getAssets().open(Chapters.LIST_BACKGROUND), ""));
+        } catch (IOException e) {
+            Log.e(TAG, "onCreate: ", e);
+        }
+
+
         recyclerView = findViewById(R.id.favorites_recycler_view);
-        LinearLayoutManager manager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        LinearLayoutManager manager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(manager);
         FavoriteAdapter adapter = null;
         try {
